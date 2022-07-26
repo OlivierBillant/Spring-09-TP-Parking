a402a27adc418ddfc58428c06af08b5615e21a2e
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-07-26 11:08:20 +0200

    CREATE

---
+++ src/main/java/fr/eni/Spring09TPParking/Spring09TpParkingApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.Spring09TPParking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring09TpParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring09TpParkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/eni/Spring09TPParking/Spring09TpParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.Spring09TPParking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring09TpParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
